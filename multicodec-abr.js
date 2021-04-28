a323f44771888e1efd1992d375c910419bd0d1d2
Hadar Bejerano <hadarbj@gmail.com>
2021-04-28 14:04:55 +0300

    Update multicodec-abr.js

--- multicodec-abr.js
+++ multicodec-abr.js
@@ -3,9 +3,14 @@ var cld = cloudinary.Cloudinary.new({
   });
   var src = {
     publicId: "bur-4k",
-    sourceTypes: ["hls/hvc1", "hls"],
+    sourceTypes: ["dash/vp9", "hls/h265", "hls"],
     sourceTransformation: {
-        "hls/hvc1": [
+        "dash/vp9": [
+          {
+            streaming_profile: "4k_vp9"
+          }
+      ],
+        "hls/h265": [
           {
             streaming_profile: "4k_h265"
           }
