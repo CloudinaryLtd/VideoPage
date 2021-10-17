ca1dae84276f1116f3baae3d7f81d6c55b407feb
Hadar Bejerano <hadarbj@gmail.com>
2021-10-17 16:13:46 +0300

    Create abr.js

---
+++ hadar/abr.js
@@ -0,0 +1,22 @@
+var cld = cloudinary.Cloudinary.new({
+    cloud_name: "hadard"
+  });
+  var src = {
+    publicId: "bur-4k",
+    sourceTypes: ["hls/hvc1", "hls"],
+    sourceTransformation: {
+        "hls/hvc1": [
+          {
+            streaming_profile: "full_hd_h265"
+          }
+        ],
+        "hls": [
+          {
+            streaming_profile: "full_hd"
+          }
+        ]
+      } 
+  };
+  var player = cld.videoPlayer("example-player");
+  player.posterOptions({ transformation: { quality: 'auto' , fetchFormat: 'auto', width: 1920, crop: 'limit'} })
+  player.source(src);
