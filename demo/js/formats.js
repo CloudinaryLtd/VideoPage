93fdd833eb362a0342491d4ada38c8fe0f997939
Hadar <hadar@cloudinary.com>
2022-12-04 13:03:41 +0200

    Update source dynamically

--- demo/js/formats.js
+++ demo/js/formats.js
@@ -1,6 +1,8 @@
 const resolutions = [1920,1280,960, 640, 480, 320];
 const video_collection = document.getElementsByTagName("video");
 const ar_coef = (16 / 9);
+const src_base = "https://res.cloudinary.com/demo/video/upload/c_limit,q_auto,w_";
+const src_file = "/sea_turtle."
 resolutions.forEach(setOnClick)
 
 function setOnClick(resolution) {
@@ -11,6 +13,6 @@ function setResolutions(resolution) {
     for (let i = 0; i < video_collection.length; i++) {
         video_collection[i].width = resolution
         video_collection[i].height = resolution / ar_coef;
-        console.log(resolution / ar_coef);
+        video_collection[i].src = (src_base + resolution.toString() + "," + video_collection[i].dataset.codec + src_file + video_collection[i].dataset.format);
     }
   } 
